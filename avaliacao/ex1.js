ab13ce9e5a953910b758702cb8913a3a31bba397
henriquerojo <henriquerojo10@gmail.com>
2023-07-27 15:42:57 -0300

    enunciado ex1

---
+++ avaliacao/ex1.js
@@ -0,0 +1,16 @@
+/*
+
+Faça um programa que receba a média de um aluno
+Caso a média seja < 5 imprima 'Reprovado'
+Caso a média seja >= 5 e < 7 imprima 'Recuperação'
+Caso a média seja >= 7 imprima 'Aprovado'
+
+Exemplo:
+
+Entrada:
+5.5
+Saída:
+Recuperação
+
+*/
+
